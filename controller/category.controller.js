d59316263fbd081ed96ff28a466e0f0dfc1ccc09
Divya <divyawaghmare2@gmail.com>
2022-04-01 10:41:26 +0530

    category

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -4,15 +4,60 @@ const { response } = require("express");
 exports.addcategory = (request, response, next) => {
   Category.create({
     categoryname: request.body.categoryname,
-    categoryimage: "http://localhost:3000/images/" + request.file.filename,
+    categoryimage: "https://angularbackendapi.herokuapp.com/images"/ + request.file.filename,
   })
     .then((result) => {
       return response.status(201).json(result);
     })
     .catch((err) => {
       console.log(err);
-      return response
-        .status(403)
-        .json({ message: "opps something went wrong!" });
+      return response.status(500).json({ message: "opps something went wrong!" });
     });
   }
+
+  exports.viewcategory=(request,response,next)=>{
+    Category.find()
+    .then(result=>{
+      return response.status(201).json(result);
+    }).catch(err=>{
+      console.log(err);
+      return response.status(500).json({error:"Something went wrong"});
+    });
+  }
+
+  // exports.deletecategory=(request,response,next)=>{
+  //   Category.deleteOne({_id:request.params.id}
+  //     .then(result=>{
+  //       console.log(result);
+  //       if(result.deletedCount)
+  //        return response.status(201).json({message:"Deleted Success"});
+  //        else
+  //        return response.status(204).json({message:"Not Deleted"});
+  //     }).
+  //     catch(err=>{
+  //       console.log(err);
+  //       return response.status(500).json({error:"Oops! Something went wrong"});
+  //     })
+  //     )
+  // }
+
+  // exports.updatecategory=(request,response,next)=>{
+  //  Category.updateOne({_id:request.params.id},
+  //     {
+  //       $set:{
+  //         categoryname:request.body.categoryname,
+  //         categoryimage:"http://localhost:3000/images/"+request.file.filename
+  //       }
+  //   })
+  //  .then(result=>{
+  //    console.log(result);
+  //    if(result.modifiedCount)
+  //     return response.status(201).json({message:"Updated successs"});
+  //     else
+  //      return response.status(204).json({message:"Not Success"});
+  //  })
+  //  .catch(err=>{
+  //    console.log(err);
+  //    return response.status(500).json({error:"Something went wrong"});
+  //  })
+  // }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const userController=require('../controller/user.controller');
 const categoryController=require('../controller/category.controller.js');
+// const productController=require('../controller/product.controller');
 const multer = require("multer");
 var Storage = multer.diskStorage({
   destination: "public/images",
@@ -12,8 +13,14 @@ var Storage = multer.diskStorage({
 var upload = multer({ storage: Storage });
 
 router.post("/signup" ,userController.signup);
-
 router.post("/signin" ,userController.signin);
+
 router.post("/addcatgeory",upload.single("categoryimage"),categoryController.addcategory); 
+router.get("/viewcategory",categoryController.viewcategory);
+// router.delete("/deletecategory/:id",categoryController.deletecategory);
+// router.post("/updatecategory/:id",upload.single("categoryimage"),categoryController.updatecategory);
+
+
 
 module.exports = router;
+
