d19972a96f4b3ea27de1ee45801cd1b23467b6f7
Divya <divyawaghmare2@gmail.com>
2022-04-02 16:22:23 +0530

    finalyvarification

--- middleware/tokenVarification.js
+++ middleware/tokenVarification.js
@@ -1,20 +1,18 @@
-const { response, request } = require('express');
 const jwt=require('jsonwebtoken');
 
 exports.tokenvarify=(request,response,next)=>{
     try{
-        console.log(request.headers.Authorization)
-        if(!request.headers.Authorization)
+        if(!request.headers.authorization)
         return response.status(401).send("UnAuthorized request 1 ");
-        if(request.headers.Authorization == null)
+        if(request.headers.authorization == null)
         return response.status(401).send("UnAuthorized request 2 ");
  
-        let token =request.headers.Authorization.split(" ")[1];
-        let payload=jwt.verify(token,"hjdjshfdhsjhf");
-        console.log(payload);
+        let token =request.headers.authorization.split(" ")[1];
+        let Payload=jwt.verify(token,"hjdjshfdhsjhf");
         next();
     }
     catch(err){
+        console.log(err);
         return response.status(401).send("UnAuthorized request");
     }
      
